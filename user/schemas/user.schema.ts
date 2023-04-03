491d7698754387497f15bbe11adadc50fc90462c
Zakaria-zaki <03zaki95@gmail.com>
2023-04-03 04:10:46 +0200

    Create user schema

--- user/schemas/user.schema.ts
+++ user/schemas/user.schema.ts
@@ -1,16 +1,21 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
+export type UserDocument = User & Document;
 
-@Schema({
-  timestamps: { createdAt: 'created', updatedAt: 'updated' },
-})
+@Schema()
 export class User {
   @Prop({ required: true })
   name: string;
 
+  @Prop({ required: true, unique: true })
+  username: string;
+
   @Prop({ required: true })
   password: string;
+
+  @Prop()
+  refreshToken: string;
 }
 
-export const MenuSchema = SchemaFactory.createForClass(User);
+export const UserSchema = SchemaFactory.createForClass(User);

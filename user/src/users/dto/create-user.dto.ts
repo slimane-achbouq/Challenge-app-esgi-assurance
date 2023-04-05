a9ddcb399f457a33b67796f2301769ce1f344370
Zakaria-zaki <03zaki95@gmail.com>
2023-04-05 06:05:53 +0200

    Create dto for user module

---
+++ user/src/users/dto/create-user.dto.ts
@@ -0,0 +1,45 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  firstname: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  lastname: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  @MinLength(8)
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  adresse: string;
+
+  @IsString()
+  @IsNotEmpty()
+  city: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  codeCity: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  age: number;
+
+  refreshToken: string;
+}

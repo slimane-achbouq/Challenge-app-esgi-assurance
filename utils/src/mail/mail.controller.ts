d4f9c87168d411e2da9cd05d740e80d0480a716a
Zakaria-zaki <03zaki95@gmail.com>
2023-04-07 15:24:50 +0200

    add mail module

--- utils/src/mail/mail.controller.ts
+++ utils/src/mail/mail.controller.ts
@@ -1,8 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { MailService } from './mail.service';
+import { VerifyDto } from './dto/verify-profile.dto';
 
 @ApiTags('Mail')
 @Controller({
   path: 'mail',
 })
-export class MailController {}
+export class MailController {
+  constructor(private readonly maileService: MailService) {}
+
+  @Post('verify')
+  sendMailtoValidateUser(@Body() veridyDto: VerifyDto) {
+    return this.maileService.sendMailtoValidateUser(veridyDto);
+  }
+}

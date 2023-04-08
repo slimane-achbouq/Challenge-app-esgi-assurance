9f553cfa17cee6e547808a82bfd4e37a1ff2941a
Zakaria-zaki <03zaki95@gmail.com>
2023-04-08 04:29:36 +0200

    Change send mail service from mailer to send in blue

--- utils/src/mail/mail.service.ts
+++ utils/src/mail/mail.service.ts
@@ -1,54 +1,75 @@
-import { MailerService } from '@nestjs-modules/mailer';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { join } from 'path';
+import * as path from 'path';
+
 import * as fs from 'fs';
 import { MailVerifyProfile } from 'src/models/mail-verify-profile.model';
+import * as SibApiV3Sdk from 'sib-api-v3-sdk';
 
 @Injectable()
 export class MailService {
   templateFolderPath = join(__dirname, 'templates');
-  constructor(private mailerService: MailerService) {}
 
   async sendMailtoValidateUser(verifyProfileModel: MailVerifyProfile) {
-    const templateFolderName = 'verify-user-email';
-    // const templatePath = join(
-    //   this.templateFolderPath,
-    //   templateFolderName,
-    //   `body.hbs`,
-    // );
-    // const subject = this.getEmailSubject(
-    //   templateFolderName,
-    //   verifyProfileModel.token,
-    // );
+    const templatePath = './src/mail/templates/verify-user-email/body.hbs';
+    const subject = fs.readFileSync(
+      './src/mail/templates/verify-user-email/header.txt',
+      { encoding: 'utf8' },
+    );
+    const templateContent = fs.readFileSync(templatePath, { encoding: 'utf8' });
 
     const url = 'localehost:3000/auth/verify/' + verifyProfileModel.token;
 
-    await this.mailerService.sendMail({
-      to: verifyProfileModel.email,
-      subject: 'zakaria',
-      template: './src/mail/templates/verify-user-email/body.hbs',
-      context: {
+    const sendSmtpEmail = {
+      sender: {
+        email: process.env.MAIL_USER,
+      },
+      subject: subject,
+      to: [
+        {
+          email: `${verifyProfileModel.email}`,
+        },
+      ],
+      htmlContent: templateContent,
+      params: {
         url: url,
       },
-    });
+      headers: {
+        'X-Mailin-custom':
+          'custom_header_1:custom_value_1|custom_header_2:custom_value_2',
+      },
+    };
+
+    try {
+      await this.sendEmail(sendSmtpEmail);
+    } catch (error) {
+      console.log(error);
+    }
+    return true;
   }
 
   /**
-   * Edit header of the e-mail
-   *
-   * @param templateFolderName
-   * @param token
-   * @private
+   * Send Mail message which contains broadcast link
+   * @param sendSmtpEmail
    */
-  private getEmailSubject(templateFolderName: string, token: string): string {
-    const subjectPath = join(
-      this.templateFolderPath,
-      templateFolderName,
-      `header.txt`,
-    );
-    const subjectTemplate = fs.readFileSync(subjectPath, { encoding: 'utf8' });
-    const subject = subjectTemplate.replace('{{organization}}', token);
+  async sendEmail(sendSmtpEmail: any) {
+    const defaultClient = SibApiV3Sdk.ApiClient.instance;
 
-    return subject;
+    const apiKey = defaultClient.authentications['api-key'];
+    apiKey.apiKey = process.env.SEND_IN_BLUE_API_KEY;
+
+    const partnerKey = defaultClient.authentications['partner-key'];
+    partnerKey.apiKey = process.env.SEND_IN_BLUE_API_KEY;
+
+    const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+    apiInstance.sendTransacEmail(sendSmtpEmail).then(
+      function () {
+        Logger.log('Send mail successfully.');
+      },
+      function (error) {
+        Logger.error(error);
+      },
+    );
   }
 }

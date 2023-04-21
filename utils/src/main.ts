2bc24b92bf8c4f7016022ac1eeee1c70de4a6ffc
Zakaria-zaki <03zaki95@gmail.com>
2023-04-21 11:08:17 +0200

    Docker configuration for microservices, Adapt Utils microService

--- utils/src/main.ts
+++ utils/src/main.ts
@@ -7,36 +7,43 @@ import {
   SwaggerModule,
 } from '@nestjs/swagger';
 import { MailModule } from './mail/mail.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: 3004,
+    },
+  });
 
   // Validation
   app.useGlobalPipes(new ValidationPipe());
 
   // Swagger config
-  const swaggerCustomOptions: SwaggerCustomOptions = {
-    customSiteTitle: 'HTTP API CHALLENGE',
-    swaggerOptions: {
-      docExpansion: 'none',
-      persistAuthorization: true,
-    },
-  };
+  // const swaggerCustomOptions: SwaggerCustomOptions = {
+  //   customSiteTitle: 'HTTP API CHALLENGE',
+  //   swaggerOptions: {
+  //     docExpansion: 'none',
+  //     persistAuthorization: true,
+  //   },
+  // };
 
-  const config = new DocumentBuilder()
-    .setTitle('Nest Project')
-    .setContact('API HTTP Project', 'esgi-5IW2.fr', '5IW2-Groupe-5@contact.com')
-    .setDescription(`Nest utils`)
-    .setVersion('1')
-    .build();
+  // const config = new DocumentBuilder()
+  //   .setTitle('Nest Project')
+  //   .setContact('API HTTP Project', 'esgi-5IW2.fr', '5IW2-Groupe-5@contact.com')
+  //   .setDescription(`Nest utils`)
+  //   .setVersion('1')
+  //   .build();
 
-  const doc = SwaggerModule.createDocument(app, config, {
-    include: [AppModule, MailModule],
-  });
-  SwaggerModule.setup('api', app, doc, swaggerCustomOptions);
+  // const doc = SwaggerModule.createDocument(app, config, {
+  //   include: [AppModule, MailModule],
+  // });
+  // SwaggerModule.setup('api', app, doc, swaggerCustomOptions);
 
-  await app.listen(process.env.APP_PORT || 3000);
-  const welcomeLog = `Application is running on: ${await app.getUrl()}`;
+  await app.listen();
+  const welcomeLog = `Application is running on: 3004}`;
   Logger.log(welcomeLog, 'NestApplication');
 }
 bootstrap();

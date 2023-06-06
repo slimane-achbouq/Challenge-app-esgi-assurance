0625c562a5eb304e4a6cdec9d70dc93787c3d4cc
Zakaria-zaki <03zaki95@gmail.com>
2023-06-06 15:41:53 +0200

    Configure claims ms with rabitmq

--- claims/src/main.ts
+++ claims/src/main.ts
@@ -1,46 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
-import {
-  SwaggerCustomOptions,
-  DocumentBuilder,
-  SwaggerModule,
-} from '@nestjs/swagger';
-import { DemandModule } from './demand/demand.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  // Swagger config
-  const swaggerCustomOptions: SwaggerCustomOptions = {
-    customSiteTitle: 'HTTP API CHALLENGE',
-    swaggerOptions: {
-      docExpansion: 'none',
-      persistAuthorization: true,
+  // const app = await NestFactory.create(AppModule);
+  // await app.listen(3006);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://admin:admin_password@rabbitmq:5672'],
+        queue: 'claims_service_queue',
+        queueOptions: { durable: false },
+      },
     },
-  };
+  );
 
-  const config = new DocumentBuilder()
-    .setTitle('API HTTP Project')
-    .setContact(
-      'API HTTP Project',
-      'esgi-5IW2.fr',
-      '5IW2-Groupe-28@contact.com',
-    )
-    .setDescription(`Nest summary`)
-    .setVersion('1')
-    .addBearerAuth({
-      description: `[just text field] Please enter token in following format: Bearer <JWT>`,
-      name: 'Authorization',
-      bearerFormat: 'Bearer',
-      scheme: 'Bearer',
-      type: 'http',
-      in: 'Header',
-    })
-    .build();
-
-  const doc = SwaggerModule.createDocument(app, config, {
-    include: [AppModule, DemandModule],
-  });
-  SwaggerModule.setup('api/v1', app, doc, swaggerCustomOptions);
-  await app.listen(3006);
+  app.listen().then(() => Logger.log('Microservice listening on RabbitMQ'));
 }
 bootstrap();
--- utils/src/main.ts
+++ utils/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
@@ -14,6 +15,6 @@ async function bootstrap() {
       },
     },
   );
-  app.listen().then(() => console.log('Microservice listening on RabbitMQ'));
+  app.listen().then(() => Logger.log('Microservice listening on RabbitMQ'));
 }
 bootstrap();

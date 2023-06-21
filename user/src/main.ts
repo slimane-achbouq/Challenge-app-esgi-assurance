1544042850283002297076ec46fc0608cc375b28
Slimane Achbouq <slimane321@gmail.com>
2023-06-21 22:25:34 +0200

    change dto user

--- user/src/main.ts
+++ user/src/main.ts
@@ -1,21 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.RMQ,
-      options: {
-        urls: ['amqp://admin:admin_password@rabbitmq:5672'],
-        queue: 'user_service_queue',
-        queueOptions: { durable: false },
-      },
-    },
-  );
-  app
-    .listen()
-    .then(() => console.log('User microservice listening on RabbitMQ'));
+    const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+        AppModule,
+        {
+            transport: Transport.RMQ,
+            options: {
+                urls: ['amqp://admin:admin_password@rabbitmq:5672'],
+                queue: 'user_service_queue',
+                queueOptions: { durable: false },
+            },
+        },
+    );
+    app
+        .listen()
+        .then(() => Logger.log('User microservice listening on RabbitMQ'));
 }
 bootstrap();

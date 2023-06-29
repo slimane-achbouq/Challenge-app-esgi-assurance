de266385960338026355a8629b408ffa1f8ebba7
ZaidAlaaHazim <zaid.anaee@yahoo.com>
2023-06-29 14:49:48 +0200

    logs payment

--- payment/src/app.module.ts
+++ payment/src/app.module.ts
@@ -4,12 +4,15 @@ import { AppService } from './app.service';
 import {PaymentModule} from "./payment/payment.module";
 import {MongooseModule} from "@nestjs/mongoose";
 import { ConfigModule } from '@nestjs/config';
+import {WinstonModule} from "nest-winston";
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URI),
     PaymentModule,
+    WinstonModule.forRoot({
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],

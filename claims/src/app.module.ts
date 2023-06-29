31642f8cf9812c0b7d5bcb83519eaf85435b527f
ZaidAlaaHazim <zaid.anaee@yahoo.com>
2023-06-29 11:25:58 +0200

    logs claim

--- claims/src/app.module.ts
+++ claims/src/app.module.ts
@@ -4,12 +4,15 @@ import { AppService } from './app.service';
 import { DemandModule } from './demand/demand.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import {WinstonModule} from "nest-winston";
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URI),
     DemandModule,
+    WinstonModule.forRoot({
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],

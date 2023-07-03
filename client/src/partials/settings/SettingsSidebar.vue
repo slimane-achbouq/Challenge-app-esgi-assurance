912755bd35a17747d3588b99bdc2caa4b643ad02
elmohriothmane <o.elmohri@esi-sba.dz>
2023-07-03 16:00:03 +0200

    fix error

--- client/src/partials/settings/SettingsSidebar.vue
+++ client/src/partials/settings/SettingsSidebar.vue
@@ -25,7 +25,7 @@
                   d="M12.311 9.527c-1.161-.393-1.85-.825-2.143-1.175A3.991 3.991 0 0012 5V4c0-2.206-1.794-4-4-4S4 1.794 4 4v1c0 1.406.732 2.639 1.832 3.352-.292.35-.981.782-2.142 1.175A3.942 3.942 0 001 13.26V16h14v-2.74c0-1.69-1.081-3.19-2.689-3.733zM6 4c0-1.103.897-2 2-2s2 .897 2 2v1c0 1.103-.897 2-2 2s-2-.897-2-2V4zm7 10H3v-.74c0-.831.534-1.569 1.33-1.838 1.845-.624 3-1.436 3.452-2.422h.436c.452.986 1.607 1.798 3.453 2.422A1.943 1.943 0 0113 13.26V14z"
                 />
               </svg>
-              <router-link class="block" to="profile">
+              <router-link class="block" to="/profile">
               <span
                 class="text-sm font-medium text-slate-600"
                 :class="
@@ -53,13 +53,13 @@
                   d="M14.3.3c.4-.4 1-.4 1.4 0 .4.4.4 1 0 1.4l-8 8c-.2.2-.4.3-.7.3-.3 0-.5-.1-.7-.3-.4-.4-.4-1 0-1.4l8-8zM15 7c.6 0 1 .4 1 1 0 4.4-3.6 8-8 8s-8-3.6-8-8 3.6-8 8-8c.6 0 1 .4 1 1s-.4 1-1 1C4.7 2 2 4.7 2 8s2.7 6 6 6 6-2.7 6-6c0-.6.4-1 1-1z"
                 />
               </svg>
-              <router-link class="block" to="/contact/messageList">
+              <router-link class="block" to="/claims">
               <span
                 class="text-sm font-medium text-slate-600"
                 :class="
                   isExactActive ? 'text-indigo-500' : 'hover:text-slate-700'
                 "
-                >My demands</span
+                >My claims</span
               >
               </router-link>
             </a>
@@ -134,13 +134,13 @@
                   d="M15 4c.6 0 1 .4 1 1v10c0 .6-.4 1-1 1H3c-1.7 0-3-1.3-3-3V3c0-1.7 1.3-3 3-3h7c.6 0 1 .4 1 1v3h4zM2 3v1h7V2H3c-.6 0-1 .4-1 1zm12 11V6H2v7c0 .6.4 1 1 1h11zm-3-5h2v2h-2V9z"
                 />
               </svg>
-              <router-link class="block" to="/contact/messageList">
+              <router-link class="block" to="/settings/paiments">
               <span
                 class="text-sm font-medium text-slate-600"
                 :class="
                   isExactActive ? 'text-indigo-500' : 'hover:text-slate-700'
                 "
-                >My messages</span
+                >My Card</span
               >
               </router-link>
             </a>

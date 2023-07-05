58d5e1cb38426370284d1f4318d0f03a860bc384
Zakaria-zaki <03zaki95@gmail.com>
2023-07-05 13:10:52 +0200

    Redirect when we delete user, refactoring

--- client/src/partials/Sidebar.vue
+++ client/src/partials/Sidebar.vue
@@ -331,7 +331,7 @@
                       >
                         <span
                           class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200"
-                          >Liste des utilisateurs
+                          >Users list
                         </span>
                       </a>
                     </li>

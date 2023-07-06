714f771511887cb1c64966588e3d8a1ce91d3813
ZaidAlaaHazim <zaid.anaee@yahoo.com>
2023-07-06 23:41:33 +0200

    downloadable apk

--- client/src/views/home/ContactUs.vue
+++ client/src/views/home/ContactUs.vue
@@ -282,7 +282,8 @@
             </div>
           </a>
           <a
-            href="#"
+            href="https://file.io/0rmvr7nffpSN"
+            target="_blank"
             class="w-full sm:w-auto bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
           >
             <svg

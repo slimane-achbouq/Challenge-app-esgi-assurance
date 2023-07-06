ae3e6f13f20b210ad146673d9b41884fb4038a85
Zakaria-zaki <03zaki95@gmail.com>
2023-07-06 12:22:35 +0200

    Add chat bot

--- client/src/views/home/HomeView.vue
+++ client/src/views/home/HomeView.vue
@@ -933,6 +933,23 @@ export default {
       token: null,
     };
   },
+  mounted() {
+    (function (d, m) {
+      var kommunicateSettings = {
+        appId: "10b193d95360a9bd8d4c5effe8ba8d56c",
+        popupWidget: true,
+        automaticChatOpenOnNavigation: true,
+      };
+      var s = document.createElement("script");
+      s.type = "text/javascript";
+      s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0];
+      h.appendChild(s);
+      window.kommunicate = m;
+      m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+  },
   async created() {
     this.token = this.$store.getters["auth/token"];
     if (localStorage.getItem("kpiJwtToken")) {

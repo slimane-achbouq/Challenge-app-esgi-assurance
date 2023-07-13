25a8155e79c7fd9ba2b9e45068cb70e9ab52d955
Zakaria-zaki <03zaki95@gmail.com>
2023-07-13 11:20:43 +0200

    Delete unused things

--- api-gateway/src/auth/enum/statut.enum.ts
+++
@@ -1,4 +0,0 @@
-export enum Statut {
-  Actif = 'ACTIF',
-  Inactif = 'INACTIF',
-}
--- utils/src/models/mail-verify-profile.model.ts
+++
@@ -1,9 +0,0 @@
-export class MailVerifyProfile {
-  email: string;
-  token: string;
-
-  constructor({ email, token }: Partial<MailVerifyProfile> = {}) {
-    this.email = email;
-    this.token = token;
-  }
-}
